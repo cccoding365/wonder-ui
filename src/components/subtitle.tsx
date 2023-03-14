343ed91b1fb28ac1d305e775ee373f4c72909119
HCC <992196871@qq.com>
2023-03-14 23:03:27 +0800

    调整 subTitle 代码结构

--- src/components/subtitle.tsx
+++ src/components/subtitle.tsx
@@ -1,26 +1,24 @@
 import React from 'react'
-/*
-  subtitle:组件示例title 
-  subnotes:组件示例备注名
-  subdesc:组件示例描述
-*/
+
 export type TitleProps = {
+  // 组件示例标题
   subtitle: string
+  // 组件示例备注名
   subnotes?: string
+  // 组件示例描述
   subdesc?: JSX.Element | string
 }
+
 export default function SubTitle(props: TitleProps): JSX.Element {
   const { subtitle, subnotes, subdesc } = props
   return (
-    <div>
-      <h2 className="packagesSubTitle">
-        {subtitle}
-        <span>{subnotes}</span>{' '}
-      </h2>
+    <>
+      <h2 className="packagesSubTitle">{subtitle}<span>{subnotes}</span></h2>
       <p className="packagesSubDesc">{subdesc}</p>
-    </div>
+    </>
   )
 }
+
 SubTitle.defaultProps = {
   subnotes: '',
   subdesc: '',
