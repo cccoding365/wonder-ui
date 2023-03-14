3c966160b6bcd8b2f738bc82eb59cc4a189047fe
HCC <992196871@qq.com>
2023-03-14 22:56:13 +0800

    调整 InstanceView 代码结构

--- src/layout/instanceView.tsx
+++ src/layout/instanceView.tsx
@@ -1,55 +1,43 @@
-import React from 'react';
-import SubTitle from '../components/subtitle';
+import React from 'react'
+import SubTitle from '../components/subtitle'
+import CodeView from '../components/codeView'
 
-import CodeView from '../components/codeView';
-/**
-  * @type {(arg: {
-  *  "subtitle": string  标题
-  *  "subnotes": string  标题下的注释
-  *  "demo": ReactNode  实例
-  *  "subdesc": string  实例下的描述
-  *  "path": string  实例代码的路径
-  *  "isSubTitle": boolean  是否显示标题
-  *  "isDemo": boolean  是否显示实例
-  *  "isCode": boolean  是否显示代码
-  *  "showView": boolead  是否显示代码预览下预览按钮
-  *  "showCopy": boolean  是否显示代码预览里copy按钮
-  * })}
-  */
 export type InstanceViewProps = {
+  // 标题
   subtitle: string,
+  // 标题注释
   subnotes?: string,
+  // 标题描述
   subdesc?: JSX.Element | string,
+  // 组件实例
   demo?: JSX.Element,
+  // 是否显示标题
   isSubTitle?: boolean,
+  // 是否显示实例
   isDemo?: boolean,
+  // 是否显示代码
   isCode?: boolean,
+  // 实例代码路径
   path: string,
+  // 是否显示代码预览下预览按钮
   showView?: boolean,
+  // 是否显示代码预览里复制按钮
   showCopy?: boolean,
+  // 是否显示代码
   showCode?: boolean,
 }
+
 function InstanceView(props: InstanceViewProps) {
-  const { subtitle, subnotes, subdesc, demo, isSubTitle, isDemo, isCode, path, showView, showCopy, showCode } = props;
+  const { subtitle, subnotes, subdesc, demo, isSubTitle, isDemo, isCode, path, showView, showCopy, showCode } = props
   return (
     <div className="instanceView_inner">
-      {
-        isSubTitle ? <SubTitle
-          subtitle={subtitle}
-          subnotes={subnotes}
-          subdesc={subdesc}
-        /> : null
-      }
-
-      {
-        isDemo ? (<div className="customDemoStyle">{demo}</div>) : null
-      }
-      {
-        isCode ? <CodeView path={path} showView={showView} showCopy={showCopy} showCode={showCode} /> : null
-      }
+      {isSubTitle ? <SubTitle subtitle={subtitle} subnotes={subnotes} subdesc={subdesc} /> : null}
+      {isDemo ? <div className="customDemoStyle">{demo}</div> : null}
+      {isCode ? <CodeView path={path} showView={showView} showCopy={showCopy} showCode={showCode} /> : null}
     </div>
   )
 }
+
 InstanceView.defaultProps = {
   subnotes: '',
   subdesc: '',
@@ -61,4 +49,5 @@ InstanceView.defaultProps = {
   showCopy: true,
   showCode: false,
 }
-export default React.memo(InstanceView);
+
+export default React.memo(InstanceView)
